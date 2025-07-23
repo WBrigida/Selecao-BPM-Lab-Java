4476a26fad25e7bcb3d48206c07d7c7e57da6bac
WBrigida <wandersonbrigida15@gmail.com>
2025-07-23 00:49:13 -0300

    inicializando os exercicios propostos em sala

---
+++ src/Exercicio_1_01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio_1_01 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o valor da base: ");
+        double base = sc.nextDouble();
+        System.out.print("Digite o valor da altura: ");
+        double altura = sc.nextDouble();
+
+        double area = base * altura;
+        System.out.println("Area = " + area);
+
+        sc.close();
+    }
+}
